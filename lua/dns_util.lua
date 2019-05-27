6619024ece27b8517ddb9949aa2b11343b6346e6
Tuan Vuong <vanhtuan0409@gmail.com>
2019-05-27 22:21:00 +0700

    add config for dns resolver

---
+++ lua/dns_util.lua
@@ -0,0 +1,62 @@
+local resolver = require("resty.dns.resolver")
+
+local _M = {}
+
+local function a_records(answers)
+  local addresses = {}
+
+  for _, ans in ipairs(answers) do
+    if ans.address then
+      table.insert(addresses, ans.address)
+    end
+  end
+
+  return addresses
+end
+
+local function resolve_host(host, r, qtype)
+  local answers
+  answers, err = r:query(host, { qtype = qtype }, {})
+  if not answers then
+    return nil, tostring(err)
+  end
+
+  if answers.errcode then
+    return nil, string.format("server returned error code: %s: %s", answers.errcode, answers.errstr)
+  end
+
+  local addresses = a_records(answers)
+  if #addresses == 0 then
+    return nil, "no record resolved"
+  end
+
+  return addresses, nil
+end
+
+function _M.resolve(host, nameservers)
+  local r
+  r, err = resolver:new{
+    nameservers = nameservers,
+    retrans = 5,
+    timeout = 2000,  -- 2 sec
+  }
+
+  if not r then
+    ngx.log(ngx.ERR, "failed to instantiate the resolver: " .. tostring(err))
+    return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+  end
+
+  local addresses
+  addresses, err = resolve_host(host, r, r.TYPE_A)
+  if not addresses then
+    ngx.log(ngx.ERR, "failed to query the DNS server: " .. tostring(err))
+    return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+  elseif #addresses == 0 then
+    ngx.log(ngx.ERR, "DNS resolve to empty result")
+    return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+  end
+
+  ngx.ctx.upstream_pods = addresses
+end
+
+return _M
